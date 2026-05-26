4a4a3590db4e4a0c8fbab395eeb1a3e544fdf60c
Qiao Xinyang <issv450@nus.edu.sg>
2026-05-26 16:14:00 +0800

    Initialize Spring Boot project

---
+++ src/test/java/sg/edu/nus/secure_api/SecureApiApplicationTests.java
@@ -0,0 +1,13 @@
+package sg.edu.nus.secure_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecureApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
